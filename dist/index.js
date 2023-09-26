4de8eb3074df16d6bbb8b265061d8b538a25d2d2
Rajan David <rajandavid.t@cloudnowtech.com>
2023-09-26 13:01:40 +0530

    simple login form added

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,62 @@
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
-var React = _interopDefault(require('react'));
+var React = require('react');
+var React__default = _interopDefault(React);
 
-var styles = {"test":"_styles-module__test__3ybTi"};
+var styles = {"loginForm":"_styles-module__loginForm__1VXj7","formGroup":"_styles-module__formGroup__3CEBj"};
+
+var LoginForm = function LoginForm(_ref) {
+  var className = _ref.className,
+    onSubmit = _ref.onSubmit;
+  var _useState = React.useState(''),
+    email = _useState[0],
+    setEmail = _useState[1];
+  var _useState2 = React.useState(''),
+    password = _useState2[0],
+    setPassword = _useState2[1];
+  var handleEmailChange = function handleEmailChange(e) {
+    setEmail(e.target.value);
+  };
+  var handlePasswordChange = function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  };
+  var handleSubmit = function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit({
+      email: email,
+      password: password
+    });
+  };
+  return /*#__PURE__*/React__default.createElement("form", {
+    className: styles.loginForm + " " + className,
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/React__default.createElement("div", {
+    className: styles.formGroup
+  }, /*#__PURE__*/React__default.createElement("label", {
+    htmlFor: "email"
+  }, "Email:"), /*#__PURE__*/React__default.createElement("input", {
+    type: "email",
+    id: "email",
+    value: email,
+    onChange: handleEmailChange
+  })), /*#__PURE__*/React__default.createElement("div", {
+    className: styles.formGroup
+  }, /*#__PURE__*/React__default.createElement("label", {
+    htmlFor: "password"
+  }, "Password:"), /*#__PURE__*/React__default.createElement("input", {
+    type: "password",
+    id: "password",
+    value: password,
+    onChange: handlePasswordChange
+  })), /*#__PURE__*/React__default.createElement("button", {
+    type: "submit"
+  }, "Login"));
+};
 
 var ExampleComponent = function ExampleComponent(_ref) {
-  var text = _ref.text;
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/React__default.createElement("div", {
     className: styles.test
-  }, "Example Component: ", text);
+  }, /*#__PURE__*/React__default.createElement(LoginForm, null));
 };
 
 exports.ExampleComponent = ExampleComponent;
--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-var styles = {"test":"_styles-module__test__3ybTi"};
+var styles = {"loginForm":"_styles-module__loginForm__1VXj7","formGroup":"_styles-module__formGroup__3CEBj"};
+
+const LoginForm = ({
+  className,
+  onSubmit
+}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({
+      email,
+      password
+    });
+  };
+  return /*#__PURE__*/React.createElement("form", {
+    className: `${styles.loginForm} ${className}`,
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/React.createElement("div", {
+    className: styles.formGroup
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "email"
+  }, "Email:"), /*#__PURE__*/React.createElement("input", {
+    type: "email",
+    id: "email",
+    value: email,
+    onChange: handleEmailChange
+  })), /*#__PURE__*/React.createElement("div", {
+    className: styles.formGroup
+  }, /*#__PURE__*/React.createElement("label", {
+    htmlFor: "password"
+  }, "Password:"), /*#__PURE__*/React.createElement("input", {
+    type: "password",
+    id: "password",
+    value: password,
+    onChange: handlePasswordChange
+  })), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Login"));
+};
 
 const ExampleComponent = ({
   text
 }) => {
   return /*#__PURE__*/React.createElement("div", {
     className: styles.test
-  }, "Example Component: ", text);
+  }, /*#__PURE__*/React.createElement(LoginForm, null));
 };
 
 export { ExampleComponent };
